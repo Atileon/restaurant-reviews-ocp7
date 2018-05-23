de1a6a65573798853a4578375341126ef0d48ae1
Steve N. Peralta R.(Atileon) <atileon.v.a@gmail.com>
2018-05-23 14:40:41 +0200

    switch markers on list items

--- js/avaMap.js
+++ js/avaMap.js
@@ -1,7 +1,7 @@
 //=========== Review Restaurants Project 7 Openclassrooms  ==============
 
 class Restaurant {
-   constructor(ristObject){
+   constructor(ristObject,markArr){
       this.listCont = 'list-container';//Main list Container
       this.descrClass = 'risto-descr'; //Class for description container (name & address)
       this.itemClass = 'risto-item';// Class for the item
@@ -15,6 +15,9 @@ class Restaurant {
       this.address = this.ristObj.vicinity;
       this.stars = this.ristObj.rating;
       this.id = this.ristObj.place_id;
+
+      this.markArr = markArr;
+      this.switchMarker = 0;
    }
    ristoItem(){
       // The container
@@ -45,16 +48,31 @@ class Restaurant {
       descrEl.appendChild(nameEl);
       descrEl.appendChild(addressEl);
       itemEl.appendChild(starsEl);
-      
-      itemEl.addEventListener('mouseenter',()=>{
-        console.log(itemEl.dataset.placeId);
+      this.switchMarker = 0;
+      itemEl.addEventListener('click',(e)=>{
+        if(e.type==='click'){
+          this.switchMarker++;
+        }
+        console.log(this.switchMarker);
+          if(this.switchMarker === 1){
+            this.ristoMark(true);
+            itemEl.classList.add('selected');
+          }else if(this.switchMarker === 2){
+            itemEl.classList.remove('selected');
+            this.markArr.map(mark => mark.setMap(null));
+            this.markArr = []
+            this.switchMarker = 0;
+          }
+          console.log(this.switchMarker);
+        
       });
+      // itemEl.addEventListener('')
       console.log(`${this.name} has been Rendered On DOM`);
    }
    
-   ristoMark(){
-      let pos = this.ristObj.geometry.location;
-
+   ristoMark(bool){
+      
+      let pos = this.ristObj.geometry.location; 
       let content = {
         content: `
        <div>This place is called ${this.name}, and the rating of this is ${this.stars}</div>
@@ -68,10 +86,13 @@ class Restaurant {
          position: pos,
          animation: google.maps.Animation.DROP,
          title: this.name
-      }
+      };
       let selEl = document.getElementById(this.id);
       let marker = new google.maps.Marker(markOpt);
-      marker.setMap(map);
+      this.markArr.push(marker);
+      console.log(this.markArr);
+      (bool === true)? marker.setMap(map): '';
+      (bool === false)?marker.setMap(null): '';
       marker.addListener('click',()=> {
         info.open(map,marker);
         
@@ -83,17 +104,17 @@ class Restaurant {
         }
       });
       map.addListener('click',() => {
-        
+        // this.ristoMark('off');
+        this.markArr.map(mark => mark.setMap(null));
+        this.markArr = [];
+        this.switchMarker = 0;
         info.close();
         selEl.classList.remove('selected');
       });
-
-      
-
    }
    
    
-}
+}//end Restaurant class
 
 
 let map;
@@ -101,7 +122,7 @@ let geoCoder
 let infowindow;
 let servicePlace;
 let search;
-
+let ristoArr =[];
 let userPos;
 
 (navigator.geolocation)?navigator.geolocation.getCurrentPosition(initMap, errorMessage):alert("Your browser does not support Geolocation.");
@@ -186,12 +207,14 @@ function initMap(position){
       rankBy: google.maps.places.RankBy.PROMINENCE
    }
    //So, we search for restaurants near by user position and pass it on toRestaurants() function to create restaurant objects from the response of service
+
    servicePlace.nearbySearch(nearbyReq,toRestaurants);
 
 }//End of main function
-  const restaurantArr = [];
+
   //The following function aims to retrieve restaurants near to user location and pass one by one to get info and markers
    function toRestaurants(results, status){
+     
      // This is the main if statement to verify Status of request and launch the entire app to create Restaurants objects and get details of each one
       if(status =='OK'){
         // So, We map the results to get details of each place
@@ -204,15 +227,22 @@ function initMap(position){
               
               if(status=== google.maps.places.PlacesServiceStatus.OK) {
                 console.log(place);
-                let newRisto = new Restaurant(place);
+                let newRisto = new Restaurant(place,ristoArr);
                 newRisto.ristoItem();
-                newRisto.ristoMark();
+                // newRisto.ristoMark(false);
               }
             });
 
          });
          
       }
+   }
+
+   function showMarkers(){
+     markOn();
+   }
+   function hideMarkers(){
+     markOff();
    }
 
   // function codeAddress() {
